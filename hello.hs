2a2c76246dcd6d7923863a60dcde0b5c35d828b3
EwanD3 <mail@mailinator.com>
2023-08-20 19:39:17 +0100

    syntax-in-funtions

--- hello.hs
+++ hello.hs
@@ -1,2 +1,49 @@
 infinity = [x|x<- [1..]]
 moreInfinity = [x*2| x<-[1..]]
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+
+addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+first :: (a,b,c) -> a
+first (x, _, _) = x
+
+second :: (a,b,c) -> b
+second (_,y,_) = y
+
+third :: (a,b,c) -> c
+third (_,_,z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
